0cc378540ba1c3a42120be3018cc6dd98bf6ae6a
s0rbus <soorbus@gmail.com>
2024-05-15 12:11:34 +0100

    completed idiomatic exercise

--- exercise/idiomatic/src/main.rs
+++ exercise/idiomatic/src/main.rs
@@ -8,16 +8,30 @@
 
 // Challenge: Clippy doesn't find *everything*. What else would you change to make this code better?
 
-const pi:f32=3.14159265358979323846;
-fn count_to_5()->i32{let mut foo =0;loop{if foo>pi as i32{if foo > 5{break;}}foo=foo+1;}return 5;}
-fn main() {
-println!("I can count to {}", count_to_5());
+use std::f32::consts::PI;
+
+#[allow(clippy::disallowed_names)]
+fn count_to_5() -> i32 {
+    let mut foo = 0;
+    loop {
+        if foo > PI as i32 && foo >= 5 {
+            break;
+        }
+
+        foo += 1;
+    }
+    foo
 }
+
+fn main() {
+    println!("I can count to {}", count_to_5());
+}
+
 #[cfg(test)]
 mod test {
-use super::*;
-#[test]
-fn test_counting() {
-assert_eq!(count_to_5() == 5, true);
-}
+    use super::*;
+    #[test]
+    fn test_counting() {
+        assert!(count_to_5() == 5);
+    }
 }
